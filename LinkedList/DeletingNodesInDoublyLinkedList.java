b0c9cc2eaa6b54df579fa08d31ab0e66d427390c
Ritika Ranjan <rranjan1922@gmail.com>
2021-06-24 14:42:25 +0530

    LinkedList Day2

---
+++ LinkedList/DeletingNodesInDoublyLinkedList.java
@@ -0,0 +1,12 @@
+package LinkedList;
+
+import LinkedList.LinkedList.*;
+public class DeletingNodesInDoublyLinkedList {
+	public static void main(String[] args) {
+		
+	}
+	static DLL delete(DLL node, int x){
+		if(node==null) return null;
+		while(node!=null && node.)
+	}
+}
---
+++ LinkedList/LinkedList/DLL.java
@@ -0,0 +1,11 @@
+package LinkedList.LinkedList;
+
+public class DLL {
+	public int data;
+	public Node prev;
+	public Node next;
+	public DLL(int data){
+		this.data = data;
+		prev = next = null;
+	}
+}
