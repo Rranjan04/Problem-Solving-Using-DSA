f58a51a02986de8f9b70cb7466b6bc1a3e3600b7
Ritika Ranjan <ritikar1024@gmail.com>
2021-06-24 12:01:16 +0530

    LinkedList Day2

---
+++ LinkedList/LinkedList/DesignAPhoneDirectory.java
@@ -0,0 +1,16 @@
+package LinkedList.LinkedList;
+
+public class DesignAPhoneDirectory {
+	public static void main(String[] args) {
+		
+	}
+	/*
+	 * Design a Phone Directory which supports the following operations:
+
+		get: Provide a number which is not assigned to anyone.
+		check: Check if a number is available or not.
+		release: Recycle or release a number.
+	 */
+
+	//  Design custom data structures
+}
