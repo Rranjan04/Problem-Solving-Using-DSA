b3528d6fc2bf7e5e61ca8a3491260844d0f16d70
Ritika Ranjan <rranjan1922@gmail.com>
2021-06-28 08:50:24 +0530

    Trees Day4

--- Trees/MaxProductSplittedBinaryTree.java
+++ Trees/MaxProductSplittedBinaryTree.java
@@ -2,32 +2,30 @@ package Trees;
 
 import Trees.BinaryTree.*;
 public class MaxProductSplittedBinaryTree {
+	static int m = 1000000007;
 	long max;
-    public int maxProduct(TreeNode root) {
-        max = Integer.MIN_VALUE;
-        long sum = sum(root);
-        traverseAndFind(root,sum);
-        // System.out.println(sum);
-        return (int) max;
-        
-    }
-    public void traverseAndFind(TreeNode root,long sum){
-        if(root==null) return;
-        //including in left subtree
-        if(root.left!=null){
-            traverseAndFind(root.left,sum);
-        }if(root.right!=null){
-            traverseAndFind(root.right,sum);
-        }
-        
-        long s2 =  (sum(root))%((int)Math.pow(10,9)+7);
-        long s1 = (sum-s2)%((int)Math.pow(10,9)+7);
-         // System.out.println(s2);
-        max = Math.max(max,s2*(s1));
-       
-    }
-    public long sum(TreeNode root){
-        if(root==null) return 0;
-        return root.val+sum(root.left)+sum(root.right);
-    }
+   public int maxProduct(TreeNode root) {
+	   max = Integer.MIN_VALUE;
+	   long sum = sum(root);
+	   traverseAndFind(root,sum);
+	   max =  max%m;
+	   // System.out.println(sum);
+	   return (int) max;
+	   
+   }
+   public int traverseAndFind(TreeNode root,long sum){
+	   if(root==null) return 0;
+	   //including in left subtree
+	  int l =  traverseAndFind(root.left,sum)%m;
+	  int r = traverseAndFind(root.right,sum)%m;
+		// System.out.println(s2);
+	   max = Math.max(max,l*(sum-l));
+	   max = Math.max(max,r*(sum-r));
+	   return (l+r+root.val)%m;
+	  
+   }
+   public long sum(TreeNode root){
+	   if(root==null) return 0;
+	   return root.val+sum(root.left)+sum(root.right);
+   }
 }
