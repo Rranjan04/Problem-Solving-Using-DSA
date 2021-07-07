c31d7e7c5ef63405f9c47e9487568c58d08c6c8e
Ritika Ranjan <rranjan1922@gmail.com>
2021-07-07 13:14:47 +0530

    DP Basics

--- DP/CoinChangeII.java
+++ DP/CoinChangeII.java
@@ -7,8 +7,12 @@ public class CoinChangeII {
 	static int find(int[] coins, int target){
 		int[] dp = new int[target+1];
 		//traversing the coins array first creates combination
+		/*
+		traversing 2,3,5 first ensures that 3 will always come after 2 which will ensure 3 will never come before 2
+		hence combination
+		*/
 		for(int i=0;i<coins.length;i++){
-			for(int j=0;j<target+1;j++){
+			for(int j=coins[i];j<target+1;j++){
 				if(j==0){
 					dp[j] = 1;
 				}
