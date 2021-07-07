7cf6d316d63bff88dd2988a7515f88c99d8930c6
Ritika Ranjan <rranjan1922@gmail.com>
2021-07-07 12:11:56 +0530

    Gives coin permutation

--- DP/CoinChange.java
+++ DP/CoinChange.java
@@ -5,6 +5,22 @@ public class CoinChange {
 		find(new int[]{2,3,5}, 7);
 	}
 	static int find(int[] coins, int target){
-		
+		int[] dp = new int[target+1];
+		for(int i=0;i<target+1;i++){
+			if(i==0){
+				dp[i] = 1;
+			}
+			else{
+				for(int j=0;j<coins.length;j++){
+					if(i-coins[j]>=0){
+						dp[i] += dp[i-coins[j]];
+					}
+				}
+			}
+		}
+		for(int x:dp){
+			System.out.print(x+" ");
+		}
+		return dp[target];
 	}
 }
